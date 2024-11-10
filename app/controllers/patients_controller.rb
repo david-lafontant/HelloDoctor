cb499ce37edbd7a675fec6a7bcce8067d71e7744
David Lafontant <lafontant.david@gmail.com>
2024-11-10 11:09:03 -0500

    Make the current_user as the default user id of patient

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,5 +1,5 @@
 class PatientsController < ApplicationController
-  before_action :set_patient, only: %i[ show edit update destroy ]
+  before_action :set_patient, only: %i[show edit update destroy]
 
   # GET /patients or /patients.json
   def index
@@ -22,13 +22,14 @@ class PatientsController < ApplicationController
   # POST /patients or /patients.json
   def create
     @patient = Patient.new(patient_params)
+    @patient.user_id = current_user.id
 
     respond_to do |format|
       if @patient.save
-        format.html { redirect_to patient_url(@patient), notice: "Patient was successfully created." }
+        format.html { redirect_to patient_url(@patient), notice: 'Patient was successfully created.' }
         format.json { render :show, status: :created, location: @patient }
       else
-        format.html { render :new, status: :unprocessable_entity }
+        format.html { render :new, status: :unprocessable_entity, notice: @patient.errors }
         format.json { render json: @patient.errors, status: :unprocessable_entity }
       end
     end
@@ -38,7 +39,7 @@ class PatientsController < ApplicationController
   def update
     respond_to do |format|
       if @patient.update(patient_params)
-        format.html { redirect_to patient_url(@patient), notice: "Patient was successfully updated." }
+        format.html { redirect_to patient_url(@patient), notice: 'Patient was successfully updated.' }
         format.json { render :show, status: :ok, location: @patient }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -52,19 +53,21 @@ class PatientsController < ApplicationController
     @patient.destroy
 
     respond_to do |format|
-      format.html { redirect_to patients_url, notice: "Patient was successfully destroyed." }
+      format.html { redirect_to patients_url, notice: 'Patient was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_patient
-      @patient = Patient.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def patient_params
-      params.require(:patient).permit(:first_name, :last_name, :birthdate, :sex, :address_line1, :address_line2, :city, :state, :country, :phone, :email, :user_id)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_patient
+    @patient = Patient.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def patient_params
+    params.require(:patient).permit(:first_name, :last_name, :birthdate, :sex, :address_line1, :address_line2, :city,
+                                    :state, :country, :phone, :email, :user_id)
+  end
 end
