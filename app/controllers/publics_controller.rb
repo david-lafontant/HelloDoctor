5d0d66f020ba533699049dd31b8900524ad3acc3
David Lafontant <lafontant.david@gmail.com>
2024-11-11 10:53:33 -0500

    Get data for sorting table for doctor and receptionist

--- app/controllers/publics_controller.rb
+++ app/controllers/publics_controller.rb
@@ -3,10 +3,17 @@ class PublicsController < ApplicationController
 
   def doctor
     @page = 'doctor'
+    sort_column = params[:sort] || 'created_at'
+    sort_direction = params[:direction].presence_in(%w[asc desc]) || 'desc'
+    @patients = Patient.order("#{sort_column} #{sort_direction}").page(params[:page]).per(10)
   end
 
   def receptionist
     @page = 'receptionist'
-    @patients = Patient.order(created_at: :desc).page(params[:page])
+
+    sort_column = params[:sort] || 'created_at'
+    sort_direction = params[:direction].presence_in(%w[asc desc]) || 'desc'
+
+    @patients = Patient.order("#{sort_column} #{sort_direction}").page(params[:page]).per(10)
   end
 end
