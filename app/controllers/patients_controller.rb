0b929f224491216d104bd07bf71af06b436207cc
David Lafontant <lafontant.david@gmail.com>
2024-11-12 10:05:08 -0500

    load and authorize patients resources

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,5 +1,5 @@
 class PatientsController < ApplicationController
-  before_action :set_patient, only: %i[show edit update destroy]
+  load_and_authorize_resource
 
   # GET /patients or /patients.json
   def index
